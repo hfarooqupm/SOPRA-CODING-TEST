2a2ea88cb511e6987e0ddb1fdcbdc12944e7df98
hamza farooq <h.farooq@alumnos.upm.es>
2024-03-15 18:48:40 +0100

    Se sube la funcion para calcular el sueldo de un empleado basandose en sus horas dedicadas y la tarida aplicada

---
+++ problema3/SueldoEmpleado.java
@@ -0,0 +1,27 @@
+package problema3;
+import java.util.Scanner;
+
+public class SueldoEmpleado {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce las horas que has trabajado durante la semana: ");
+        int HorasTrabajadas = scanner.nextInt();
+        System.out.println("Dime la tarifa que se aplica por hora ");
+        double tarifa = scanner.nextDouble();
+
+        double sueldo;
+
+        if (HorasTrabajadas > 40) {
+            int horasExtras = HorasTrabajadas - 40;
+            double TarifaExtraHoras=(tarifa*(1.05));
+            sueldo = (40 * tarifa) + (horasExtras * TarifaExtraHoras);
+        } else {
+            sueldo = HorasTrabajadas * tarifa;
+        }
+
+        System.out.println("El sueldo del trabajador total es lo siguiente sumando horas extras si las hay: " + sueldo);
+
+        scanner.close();
+    }
+}
